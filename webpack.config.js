54551fac9218a29823d663f1d10a72e991cfb4ca
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-06-02 17:00:21 +0300

    fix linter config 4

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ module.exports = {
   mode: 'development',
   entry: { master: path.resolve(__dirname, 'src/index.js') },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist/'),
     filename: '[name].js',
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, '/dist/'),
     },
   },
   module: {

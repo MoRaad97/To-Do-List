047959b69bb133faf10287678ec9f83cd3c0a21f
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-06-03 12:04:41 +0300

    Add function to store the tasks at local store and get tasks from local storage

--- src/add-remove.js
+++ src/add-remove.js
@@ -1,37 +1,55 @@
-// import { taskArray,  } from "./index.js";
-export {addTaskToArray,addTaskToPage,taskArray}
+import { taskArray } from "./index.js";
+export {addTaskToArray,addTaskToPage,taskArray,getTaskFromStore}
 
 // TaskArray
-let taskArray = [];
+
 // Ul list = Tasks List
 let taskList = document.querySelector('.tasks-list');
-// Add Remove class
 
 // Add Task To Array
     let addTaskToArray = (taskText) =>{
         // task info
         const task = {
-            id: Date.now(),
+            id: taskArray.length + 1,
             title: taskText,
             completed: false, 
         }
-        // push task to array
+        // Push task to array
         taskArray.push(task);
-        
-        return taskArray
+        // Add task to page 
+        addTaskToPage(taskArray)
+        // Add task to local storage 
+        addTaskToStore(taskArray)
         }  
+
+
         // Create Task in the page 
  let addTaskToPage = (taskArray) => {
-    //  let taskLi = document.querySelector('.task')
-    //  if (taskLi.completed) {
-    //      taskLi.setAttribute(checked)
-    //  };
+// check if the task completed
+    let taskLi = document.querySelectorAll('.task')
+    if (taskLi.completed) {
+        taskLi.setAttribute(checked)
+    };
+    // Add task to the page
     let taskListSave = taskList.innerHTML;
         taskArray.forEach((task) => {
             let taskListHtml = `<li class="task completed" id="${task.id}">
-            <div><input type="checkbox" /><span>${task.title}</span></div>
+            <div><input type="checkbox"/><span>${task.title}</span></div>
             <i class="fa-solid fa-ellipsis-vertical"></i>
           </li>`;
-          taskList.innerHTML = taskListSave + taskListHtml  
+          taskList.innerHTML = taskListSave + taskListHtml  ;
           });
+        
 };
+    // Add to Store 
+let addTaskToStore = (taskArray)=> {
+    window.localStorage.setItem('tasks', JSON.stringify(taskArray))
+    }
+
+let getTaskFromStore = () => {
+   let data = window.localStorage.getItem('tasks')
+    if (data) {
+        let tasks = JSON.parse(data)
+        addTaskToPage(tasks)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import './styles/index.css'
 import './styles/all.min.css'
-import { addTaskToArray,addTaskToPage,taskArray} from "./add-remove.js";
+import {addTaskToArray,getTaskFromStore} from "./add-remove.js";
 
-// export {taskArray,taskList}
-
+export {taskArray}
+let taskArray = [];
+if (localStorage.getItem('tasks')){
+    taskArray = JSON.parse(localStorage.getItem('tasks'))
+}
 
 
 let input = document.querySelector('.ph')
 let submit = document.querySelector('.enter')
 
 
+// get task from local storage
+getTaskFromStore()
 
 submit.onclick = ()=> {
   if (input.value !== "") {
     addTaskToArray(input.value);
-     addTaskToPage(taskArray)
+    
      input.value = ""
      
   }}

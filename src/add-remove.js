d03d7785f3abb8d340207a33cea007106d40b716
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-06-04 08:12:14 +0300

    Fix linters errors

--- src/add-remove.js
+++ src/add-remove.js
@@ -1,4 +1,7 @@
-export {addTaskToArray,addTaskToPage,getTaskFromStore,deleteTaskWith}
+/* eslint-disable no-use-before-define, eqeqeq */
+export {
+  addTaskToArray, addTaskToPage, getTaskFromStore, deleteTaskWith,
+};
 
 // TaskArray
 let taskArray = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -7,67 +10,58 @@ let taskArray = JSON.parse(localStorage.getItem('tasks')) || [];
 // }
 
 // Ul list = Tasks List
-let taskList = document.querySelector('.tasks-list');
+const taskList = document.querySelector('.tasks-list');
 
 // Add Task To Array
-    let addTaskToArray = (taskText) =>{
-        // task info
-        const task = {
-            id: taskArray.length + 1,
-            title: taskText,
-            completed: false, 
-        }
-       
-        // Push task to array
-        taskArray.push(task);
-        // Add task to page 
-        addTaskToPage(taskArray)
-        // Add task to local storage 
-        addTaskToStore(taskArray)
-        }  
+const addTaskToArray = (taskText) => {
+  // task info
+  const task = {
+    id: taskArray.length + 1,
+    title: taskText,
+    completed: false,
+  };
 
+  // Push task to array
+  taskArray.push(task);
+  // Add task to page
+  addTaskToPage(taskArray);
+  // Add task to local storage
+  addTaskToStore(taskArray);
+};
 
-        // Create Task in the page 
- let addTaskToPage = (taskArray) => {
-// check if the task completed
-    let taskLi = document.querySelectorAll('.task')
-    if (taskLi.completed) {
-        taskLi.setAttribute(checked)
-    };
-    // Add task to the page
-    let taskListSave = '';
-           
-    taskArray.forEach((task) => {
-        let taskListHtml = `<li class="task" task-id="${task.id}">
+// Create Task in the page
+const addTaskToPage = (taskArray) => {
+//   // Add task to the page
+  let taskListSave = '';
+
+  taskArray.forEach((task) => {
+    const taskListHtml = `<li class="task" task-id="${task.id}">
         <div><input type="checkbox"/> <input class="ph" type="text" placeholder="Add Task" value = "${task.title}" /></div>
        <i class="fa-solid fa-trash-can del"></i>
       </li>`;
-      taskListSave += taskListHtml;
-      });
-      taskList.innerHTML = taskListSave;
-        
+    taskListSave += taskListHtml;
+  });
+  taskList.innerHTML = taskListSave;
 };
-    // Add to Store 
-let addTaskToStore = (taskArray)=> {
-    window.localStorage.setItem('tasks', JSON.stringify(taskArray))
-    }
+// Add to Store
+const addTaskToStore = (taskArray) => {
+  window.localStorage.setItem('tasks', JSON.stringify(taskArray));
+};
 
-let getTaskFromStore = () => {
-   let data = window.localStorage.getItem('tasks')
-    if (data !== null) {
-        let tasks = JSON.parse(data);
-        addTaskToPage(tasks);
-    }
-}
-
-
- const deleteTaskWith = (taskId) => {
-     if (taskArray !== null){
-        taskArray = taskArray.filter((task) => task.id != taskId);
-        taskArray.forEach((e,index) => {
-            e.id = index + 1;
-        })
-        addTaskToStore(taskArray);
-     }
-  
+const getTaskFromStore = () => {
+  const data = window.localStorage.getItem('tasks');
+  if (data !== null) {
+    const tasks = JSON.parse(data);
+    addTaskToPage(tasks);
   }
+};
+
+const deleteTaskWith = (taskId) => {
+  if (taskArray !== null) {
+    taskArray = taskArray.filter((task) => task.id != taskId);
+    taskArray.forEach((e, index) => {
+      e.id = index + 1;
+    });
+    addTaskToStore(taskArray);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,42 +1,36 @@
-import './styles/index.css'
-import './styles/all.min.css'
-import {addTaskToArray,getTaskFromStore,deleteTaskWith,} from "./add-remove.js";
+import './styles/index.css';
+import './styles/all.min.css';
+import { addTaskToArray, getTaskFromStore, deleteTaskWith } from './add-remove.js';
 
+const input = document.querySelector('.ph');
+const submit = document.querySelector('.enter');
+const taskEvent = document.body.querySelector('.tasks-list');
 
-
-
-
-
-let input = document.querySelector('.ph')
-let submit = document.querySelector('.enter')
-let taskEvent = document.body.querySelector('.tasks-list')
-
-// get task from local storage on load 
-getTaskFromStore()
+// get task from local storage on load
+getTaskFromStore();
 
 // Add Task Submit Action
 // press Enter
-input.addEventListener("keypress",(e) => {
-  if (e.key === "Enter") {
-    submit.click()
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    submit.click();
   }
-})
+});
 // click Enter icon
-submit.onclick = ()=> {
-  if (input.value !== "") {
+submit.onclick = () => {
+  if (input.value !== '') {
     addTaskToArray(input.value);
-     input.value = "" 
-  }}
+    input.value = '';
+  }
+};
 
-
-  // delete 
-  taskEvent.addEventListener('click',(e) =>   {
-  if (e.target.classList.contains("fa-trash-can")) {
+// delete
+taskEvent.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-trash-can')) {
     // delete from local storage
     // localStorage.clear();
-    deleteTaskWith(e.target.parentElement.getAttribute("task-id"));
-    // delete from page 
-    e.target.parentElement.remove()
+    deleteTaskWith(e.target.parentElement.getAttribute('task-id'));
+    // delete from page
+    e.target.parentElement.remove();
   }
-})
-
+});

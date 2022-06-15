a9d863d6f3362c9227e4720184c73992ba0bfab1
efoosag <96612103+efoosag@users.noreply.github.com>
2022-06-15 16:15:26 +0100

    fix linters error

--- __tests__/taskone.test.js
+++ __tests__/taskone.test.js
@@ -1,25 +1,23 @@
-import {addTaskToPage, deleteItem} from '../src/add-remove'
+import { addTaskToPage, deleteItem } from '../src/add-remove.js';
 
 describe('Add & Remove One Element Test', () => {
   // Arranged
-   document.body.innerHTML ='<ul class="tasks-list" id="tasks-list"></ul>';
-   const task = [{ id: 1, title: "taskText", completed: false,}]
+  document.body.innerHTML = '<ul class="tasks-list" id="tasks-list"></ul>';
+  const task = [{ id: 1, title: 'taskText', completed: false }];
   test('Add one new item to the list', () => {
-      //   Act
-      addTaskToPage(task);
-      const list = document.querySelectorAll('#tasks-list li');
-      // ِِAssert
-      expect(list).toHaveLength(1);
+    //   Act
+    addTaskToPage(task);
+    const list = document.querySelectorAll('#tasks-list li');
+    // ِِAssert
+    expect(list).toHaveLength(1);
   });
   test('Remove one item from the list elements', () => {
-     // Arranged
-     const delBtn = document.querySelectorAll('.fa-trash-can')
-     //   Act
-     deleteItem(delBtn[0])
-     const list = document.querySelectorAll('#tasks-list li');
-     // ِِAssert
-      expect(list).toHaveLength(0);
+    // Arranged
+    const delBtn = document.querySelectorAll('.fa-trash-can');
+    //   Act
+    deleteItem(delBtn[0]);
+    const list = document.querySelectorAll('#tasks-list li');
+    // ِِAssert
+    expect(list).toHaveLength(0);
   });
-})
-
-
+});
